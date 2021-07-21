1d1721a56bdeef9c072b3afa34f824f64b6269af
nikoisonfire <nikokre@gmail.com>
2021-07-21 11:13:54 +0200

    first working tests, lots of mocks, storage mocked

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,65 +1,103 @@
-//import WebExtFeedbackPopup from "./index";
+import WebExtFeedbackPopup from "./index";
 
-/* const mock = new WebExtFeedbackPopup({
-  window,
-  "Headline",
-  "Text",
-  new Date("2021-06-17T03:24:00"),
-  "https://addons.mozilla.org/de/firefox/addon/https-everywhere/",
-  "https://chrome.google.com/webstore/detail/https-everywhere/gcbommkclmclpchllfjekcdonpmejbdp",
-  "",
-  2000
-});
+const mockStoreLinks = {
+  firefox: "https://addons.mozilla.org/de/firefox/addon/grammarly-1/",
+  chrome:
+    "https://chrome.google.com/webstore/detail/grammarly-for-chrome/kbfnbcaeplbcioakkpcpgfkobkghlhen",
+  opera: "https://addons.opera.com/de/extensions/details/opera-ad-blocker/",
+  edge: "https://www.microsoft.com/en-us/p/grammarly-for-microsoft-edge/9p59wxtbhzzm?activetab=pivot:overviewtab",
+  safari: "https://apps.apple.com/us/app/grammarly-for-safari/id1462114288",
+};
 
-test("constructor: should construct mock object", () => {
-  expect(mock).toBeDefined();
-  expect(mock._headline).not.toBeNull();
-});
+let storageMock: Record<string, unknown> = {};
 
-test("constructor: should add Modal and its children to window ", () => {
-  expect(window.document.body.innerHTML).toContain('<div class="fbm-blur">');
-  expect(window.document.body.innerHTML).toContain('<div class="fbm-modal">');
-  expect(window.document.body.innerHTML).toContain('<h3 class="fbm-headline">');
-  expect(window.document.body.innerHTML).toContain('<span class="fbm-text">');
-  expect(window.document.body.innerHTML).toContain(
-    '<button class="fbm-button">'
-  );
-});
+function getStorage(key: string) {
+  return new Promise((resolve, reject) => {
+    if (storageMock[key]) resolve(storageMock);
+    else reject("Key not found");
+  });
+}
+function setStorage(obj: any) {
+  return new Promise((resolve) => {
+    storageMock = obj;
+    resolve(true);
+  });
+}
 
-test("isShowTime: should return true", () => {
-  mock.timeout = 0;
-  expect(mock.isShowtime()).toBe(true);
-});
-
-test("isShowTime: should return false", () => {
-  mock.installDate = new Date(Date.now());
-  mock.timeout = 999999;
-  expect(mock.isShowtime()).toBe(false);
-});
-
-test("createElement: should return HTMLElement", () => {
-  const testEl: HTMLElement = mock.createElement("div");
-  expect(testEl).not.toBeNull;
-  expect(testEl.tagName).toBe("DIV");
-});
-
-test("redirectTo: should open new window", () => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const oldWindow = mock.window;
-  const mockWindow: any = {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    open: (_link: string, _target: string) => {
-      return;
+jest.mock("webextension-polyfill-ts", () => {
+  return {
+    browser: {
+      storage: {
+        local: {
+          get: getStorage,
+          set: setStorage,
+        },
+      },
     },
   };
-
-  mock.window = mockWindow;
-  const spy = jest.spyOn(mockWindow, "open");
-  mock.redirectTo("https://www.google.de");
-
-  expect(spy).toHaveBeenCalled();
-
-  spy.mockRestore();
-  mock.window = oldWindow;
 });
- */
+
+jest.mock(
+  "url:./icons/chrome-logo.png",
+  () =>
+    new Array({
+      default: "/icons/chrome-logo.png",
+    })
+);
+
+document.body.innerHTML = `
+  <div>
+  </div>
+`;
+
+const mock = new WebExtFeedbackPopup({
+  window: window,
+  headline: "Would you mind sparing some feedback?",
+  text: "We've put a lot of love into this project, would you mind sharing your thoughts and maybe a feedback on it?",
+  installDate: new Date(2020, 3, 1, 10, 0, 0),
+  frequency: 2,
+  timeout: 20000,
+  storeLinks: mockStoreLinks,
+});
+
+describe("index text", () => {
+  test("constructor: should construct mock object", () => {
+    expect(mock).toBeDefined();
+    expect(mock).toBeInstanceOf(WebExtFeedbackPopup);
+    expect(mock._headline).not.toBeNull();
+  });
+
+  test("constructor: should add Modal and its children to window ", () => {
+    expect(window.document.body.innerHTML).toContain('<div class="fbm-modal">');
+    expect(window.document.body.innerHTML).toContain(
+      '<h3 class="fbm-headline">'
+    );
+    expect(window.document.body.innerHTML).toContain('<span class="fbm-text">');
+    expect(window.document.body.innerHTML).toContain(
+      '<button class="fbm-button">'
+    );
+  });
+
+  test("constructor: should add default light style", () => {
+    expect(document.head.innerHTML).toContainIgnoreWS(
+      "background-color:#e1462c80;"
+    );
+  });
+
+  test("constructor: should add explicit dark style", () => {
+    const shallowWindow = { ...window };
+
+    const mock2 = new WebExtFeedbackPopup({
+      window: shallowWindow,
+      headline: "Would you mind sparing some feedback?",
+      text: "We've put a lot of love into this project, would you mind sharing your thoughts and maybe a feedback on it?",
+      installDate: new Date(2020, 3, 1, 10, 0, 0),
+      theme: "dark",
+      storeLinks: mockStoreLinks,
+    });
+    expect(mock2).toBeDefined();
+    expect(shallowWindow.document.head.innerHTML).toContainIgnoreWS(
+      "background-color:#1D2D50;"
+    );
+  });
+});
