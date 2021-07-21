9c78386043af348254c7f94cc3adc24252a8eb28
nikoisonfire <nikokre@gmail.com>
2021-07-21 13:49:04 +0200

    idk

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/assetTransformer.js",
-  }
+  },
+  runner: 'jest-serial-runner'
 }
